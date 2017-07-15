6f28ad9ed1b7e506e9cbf50a90473f76a1c0fd34
Ahmet Elçik <ahmetelcik35@gmail.com>
2017-07-15 22:14:19 +0300

    Create AaType.php

---
+++ src/AaType.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+abstract class AaType
+{
+
+	const PROVIDER = 'provider';
+
+	const CATEGORY = 'category';
+
+	const PRIORTY = 'priorty';
+
+	const PACKAGE = 'package';
+
+	const TYPE = 'type';
+
+	const LANGUAGE = 'language';
+
+	const SEARCH = 'search';
+
+
+}
