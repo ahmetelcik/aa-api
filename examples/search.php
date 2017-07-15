125cb90533b771ceea983a5157ca1a68e9b2bfbc
Ahmet Elçik <ahmetelcik35@gmail.com>
2017-07-15 22:15:46 +0300

    Update search.php

--- examples/search.php
+++ examples/search.php
@@ -1,7 +1,6 @@
 <?php 
 
 require 'vendor/autoload.php';
-
 $USERNAME = "*******";
 $PASSWORD = "*******";
 
@@ -9,9 +8,9 @@ $aa = new Buki\AnadoluAgency($USERNAME, $PASSWORD);
 
 $content = $aa->time($start = date("Y-m-d", time()), $end = "NOW")
 			  ->limit(0, 10)
-			  ->filter("type", [1])
-			  ->filter("language", [1])
-			  ->filter("category", [1])
+			  ->filter(AaType::TYPE, [1])
+			  ->filter(AaType::LANGUAGE, [1])
+			  ->filter(AaType::CATEGORY"", [1])
 			  ->search();
 
 var_dump($content); // format: json
